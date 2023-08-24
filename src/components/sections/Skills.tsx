a3f3bb648973542eaf0e3c93ad8c0bf86d77b8a2
AndreChavs <79666974+AndreChavs@users.noreply.github.com>
2023-08-23 21:46:52 -0300

    correção

--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -10,28 +10,28 @@ const Skills = () => {
   return (
     <section id='skills' className={styles.section}>
         <h1 className={styles.title}>Skills</h1>
-      <Container>
+      <Container className={styles.containerSkills}>
         <Grid04>
           <h2 className={styles.subTitle}>Lenguages</h2>
           <div className={styles.images}>
             <div>
-              <Image src={html5.src} width={html5.width} height={html5.height} alt='HTML5' />
+              <Image src={html5.src} width={90} height={90} alt='HTML5' />
               <p>html 5</p>
             </div>
             <div>
-              <Image src='/images/CSS3.png' width={html5.width} height={html5.height} alt='CSS 3' />
+              <Image src='/images/CSS3.png' width={90} height={90} alt='CSS 3' />
               <p>CSS 3</p>
             </div>
             <div>
-              <Image src='/images/javascript.png' width={html5.width} height={html5.height} alt='Javascript' />
+              <Image src='/images/javascript.png' width={90} height={90} alt='Javascript' />
               <p>Javascript</p>
             </div>
             <div>
-              <Image src='/images/typescript.png' width={html5.width} height={html5.height} alt='Typescript' />
+              <Image src='/images/typescript.png' width={90} height={90} alt='Typescript' />
               <p>Typescript</p>
             </div>
             <div>
-              <Image src='/images/Rust.png' width={html5.width} height={html5.height} alt='Rust' />
+              <Image src='/images/Rust.png' width={90} height={90} alt='Rust' />
               <p>Rust</p>
             </div>                      
           </div>
@@ -40,23 +40,23 @@ const Skills = () => {
           <h2 className={styles.subTitle}>Stack</h2>
           <div className={styles.images}>
             <div>
-              <Image src='/images/react.png' width={html5.width} height={html5.height} alt='React.js' />
+              <Image src='/images/react.png' width={90} height={90} alt='React.js' />
               <p>React.js</p>
             </div>
             <div>
-              <Image src='/images/nodeJS.png' width={html5.width} height={html5.height} alt='Node.js' />
+              <Image src='/images/nodeJS.png' width={90} height={90} alt='Node.js' />
               <p>Node.js</p>
             </div>
             <div>
-              <Image src='/images/Nextjs.png' width={html5.width} height={html5.height} alt='Next.js' />
+              <Image src='/images/Nextjs.png' width={90} height={90} alt='Next.js' />
               <p>Next.js</p>
             </div>
             <div>
-              <Image src='/images/Nestjs.png' width={html5.width} height={html5.height} alt='Nest.js' />
+              <Image src='/images/Nestjs.png' width={90} height={90} alt='Nest.js' />
               <p>Nest.js</p>
             </div>
             <div>
-              <Image src='/images/Docker.png' width={html5.width} height={html5.height} alt='Docker' />
+              <Image src='/images/Docker.png' width={90} height={90} alt='Docker' />
               <p>Docker</p>
             </div>        
           </div>
@@ -65,31 +65,31 @@ const Skills = () => {
           <h2 className={styles.subTitle}>Others</h2>
           <div className={styles.images}>
             <div>
-              <Image src='/images/adobexd.png' width={html5.width} height={html5.height} alt='AdobeXD' />
+              <Image src='/images/adobexd.png' width={90} height={90} alt='AdobeXD' />
               <p>AdobeXD</p>
             </div>
             <div>
-              <Image src='/images/SASS.png' width={html5.width} height={html5.height} alt='SASS' />
+              <Image src='/images/SASS.png' width={90} height={90} alt='SASS' />
               <p>SASS</p>
             </div>   
             <div>
-              <Image src='/images/MUI.png' width={html5.width} height={html5.height} alt='Material UI' />
+              <Image src='/images/MUI.png' width={90} height={90} alt='Material UI' />
               <p>Material UI</p>
             </div>
             <div>
-              <Image src='/images/tailwindcss.png' width={html5.width} height={html5.height} alt='Tailwind' />
+              <Image src='/images/tailwindcss.png' width={90} height={90} alt='Tailwind' />
               <p>Tailwind</p>
             </div>
             <div>
-              <Image src='/images/mysql.png' width={html5.width} height={html5.height} alt='MYSQL' />
+              <Image src='/images/mysql.png' width={90} height={90} alt='MYSQL' />
               <p>MySQL</p>
             </div>
             <div>
-              <Image src='/images/mongoDB.png' width={html5.width} height={html5.height} alt='MongoDB' />
+              <Image src='/images/mongoDB.png' width={90} height={90} alt='MongoDB' />
               <p>MongoDB</p>
             </div>
             <div>
-              <Image src='/images/prisma.png' width={html5.width} height={html5.height} alt='Prisma' />
+              <Image src='/images/prisma.png' width={90} height={90} alt='Prisma' />
               <p>Prisma</p>
             </div>       
           </div>
