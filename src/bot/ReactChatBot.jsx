6cd92cd16cb006f98a2bd88a46920ac612c81dd8
AndreChavs <79666974+AndreChavs@users.noreply.github.com>
2024-05-29 16:39:14 -0300

    correcao

--- src/bot/ReactChatBot.jsx
+++ src/bot/ReactChatBot.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Chatbot from 'react-chatbot-kit'
 import 'react-chatbot-kit/build/main.css'
 import config from './config.js';
-import MessageParser from './Messageparser.js';
+import MessageParser from './MessageParser.js';
 import ActionProvider from './ActionProvider.js';
 import ButtonChat from './ButtonChat';
 import { useRouter } from 'next/router.js';
